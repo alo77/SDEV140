e1ba22a754babc67497bab5b8f56d3a82790fdc8
alo77 <110139636+alo77@users.noreply.github.com>
2025-01-16 13:17:55 -0600

    Create myinfo.py

---
+++ MOD1/Exercises/myinfo.py
@@ -0,0 +1,7 @@
+# Python program in a file named myinfo.py that prints (displays) your name, address and telephone number
+
+strName = input("Name: ")
+strAddress = input("Address: ")
+strPhone = input("Phone: ")
+
+print(strName, strAddress, strPhone)
