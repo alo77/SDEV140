33ad8276a49638354c88da08222a3a52f373de61
alo77 <110139636+alo77@users.noreply.github.com>
2025-02-14 01:17:50 -0600

    Update sciencereviewgame.py
    added to start_game function

--- sciencereviewgame.py
+++ sciencereviewgame.py
@@ -9,6 +9,8 @@ def start_game():
     game_data = load_game_data()
     first_module = game_data[0]
     first_question = first_module["questions"][0]["question"]
+    print("Loaded questiON:", first_question)
+    question_label.config(text=first_question)
 
 root = tk.Tk()
 root.title("Science Review Game")
