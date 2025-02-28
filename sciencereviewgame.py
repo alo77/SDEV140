5bea9d9ef5adecefba4d163571fefc1b655704a3
Abigail Logan <abbey@breadenfishmedia.com>
2025-02-28 14:39:12 -0600

    updated per new json game logic

--- sciencereviewgame.py
+++ sciencereviewgame.py
@@ -58,7 +58,7 @@ def start_game():
     game_data = load_game_data()
     first_module = game_data[0]
     first_question = first_module["questions"][0]["question"]
-    print("Loaded questiON:", first_question)
+    print("Loaded question:", first_question)
     question_label.config(text=first_question)
 
 root = tk.Tk()
