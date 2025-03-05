6de9852299d664cc8493257cfe21b29b5cc58c01
Abigail Logan <abbey@breadenfishmedia.com>
2025-03-04 22:12:14 -0600

    updated scorekeepingpopup update_score call

--- scorekeeperpopup.py
+++ scorekeeperpopup.py
@@ -8,6 +8,9 @@ Short Description: This program assists the review game as a scorekeeper module 
 import json
 import tkinter as tk
 from breezypythongui import EasyFrame
+from reviewgame import (
+    update_score
+)
 
 def show_custom_popup(parent, title, message):
     popup = tk.Toplevel(parent)
@@ -17,16 +20,17 @@ def show_custom_popup(parent, title, message):
     title_label = tk.Label(popup, text=title, font=("Helvetica", 14, "bold"))
     title_label.pack(pady=(10, 5))
 
-    confetti_image = tk.PhotoImage(file="confetti.png")
+    confetti_image = tk.PhotoImage(file="confetti.png", width=50, height=50)
     image_label = tk.Label(popup, image=confetti_image)
     image_label.image = confetti_image
+    image_label.put("image", to="center")
     image_label.pack(pady=10)
 
     message_label = tk.Label(popup, text=message, wraplength=250)
     message_label.pack(pady=(5,10), padx=20)
 
-    self.score_label = tk.Label(popup, text=f"Score: {self.score}")
-    self.score_label.pack(side="top", anchor="ne", padx=10, pady=10)
+    score_label = tk.Label(popup, text=f"Final Score: {update_score}")
+    score_label.pack(pady=10)
 
     ok_button = tk.Button(popup, text="OK", command=popup.destroy)
     ok_button.pack(pady=10)
